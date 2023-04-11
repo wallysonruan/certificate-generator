c203276b0e3c3dd39f883adc22e19b5528bbf384
wallyson ruan <wallyson.ruan@thoughtworks.com>
2023-04-11 20:56:07 -0300

    FEAT: Finished add image background feature for now

---
+++ setBackground.js
@@ -0,0 +1,33 @@
+const imgInput = document.querySelector("#input__img");
+
+imgInput.addEventListener("change", function (e){
+    const reader = new FileReader();
+    reader.addEventListener("load", () => {
+        addBackground(reader.result);
+    })
+    reader.readAsDataURL(this.files[0])
+})
+
+
+function addBackground(img){
+    addImgElementToImgContainer();
+
+    const backgroundElement = document.querySelector("#background-img");
+    backgroundElement.src = img;
+}
+
+function addImgElementToImgContainer(){
+    const displayer = document.querySelector("#img__container");
+
+    // Does not allow to create a new <img>.
+    if(displayer.children[0]){
+        return;
+    }
+
+    const imgElement = document.createElement("img");
+    imgElement.width = displayer.clientWidth;
+    imgElement.height = displayer.clientHeight;
+    imgElement.id = "background-img";
+
+    displayer.appendChild(imgElement);
+}
