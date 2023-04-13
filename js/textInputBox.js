7f224fb83261ecb75939a4897867d611ab728e0a
wallyson ruan <wallyson.ruan@thoughtworks.com>
2023-04-13 16:56:38 -0300

    FEAT: Added input text eraser

--- js/textInputBox.js
+++ js/textInputBox.js
@@ -4,10 +4,38 @@ addText.addEventListener("click", () => {
 })
 
 function createInputText(){
-    const inputText = document.createElement("input");
-    inputText.type = "text";
-    inputText.draggable = "true";
-    
     const displayer = document.querySelector("#second-layer");
-    displayer.appendChild(inputText);
+    const inputTextContainer = document.createElement("div");
+    const inputText = document.createElement("input");
+    const inputTextToolKit = createInputTextToolKit();
+
+    inputTextContainer.draggable = "true";
+    inputText.type = "text";
+
+    inputTextContainer.appendChild(inputTextToolKit);
+    inputTextContainer.appendChild(inputText);
+    displayer.appendChild(inputTextContainer);
+}
+
+function createInputTextToolKit(){
+    const list = document.createElement("ul");
+    const listItem = document.createElement("li");
+
+    listItem.appendChild(createInputTextEraseButton());
+    list.appendChild(listItem);
+
+    return list;
+}
+
+function createInputTextEraseButton(){
+    const button = document.createElement("button");
+
+    button.textContent = "ERASE"
+    button.classList.add("button", "button__erase");
+
+    button.addEventListener("click", (event) => {
+        event.target.parentElement.parentElement.parentElement.remove();
+    });
+
+    return button;
 }
